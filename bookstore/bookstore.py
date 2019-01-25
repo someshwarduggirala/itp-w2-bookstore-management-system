63042e738b4705684c8cb736ba57ecb69ae473d0
someshwarduggirala <someshwarduggirala@gmail.com>
2019-01-24 19:31:13 -0800

    Update bookstore.py

--- bookstore/bookstore.py
+++ bookstore/bookstore.py
@@ -1,26 +1,39 @@
 def create_bookstore(name):
-    pass
+    return {"name":name,
+            "authors":{},
+            "books":{}
+            
+            }
 
 
 def get_bookstore_name(bookstore):
-    pass
+    return bookstore["name"]
 
 
 def add_author(bookstore, name, nationality):
-    pass
-
-
+    author={'name':name, 'nationality':nationality}
+    bookstore["authors"].setdefault(name,author)
+    return bookstore["authors"][name]
+   
 def get_author_by_name(bookstore, name):
-    pass
+    return bookstore["authors"][name]
 
 
 def add_book(bookstore, title, isbn, author):
-    pass
+    book={'title':title,'isbn':isbn,'author':author}
+    bookstore['books'].setdefault(title,book)
+    return bookstore['books'][title]
 
 
 def get_book_by_title(bookstore, title):
-    pass
+    return bookstore['books'][title]
+        
 
 
 def get_books_by_author(bookstore, author):
-    pass
+    book_list=[]
+    print (bookstore)
+    for book in bookstore['books'].values():
+        if book['author'] == author:
+            book_list.append(book)
+    return book_list
